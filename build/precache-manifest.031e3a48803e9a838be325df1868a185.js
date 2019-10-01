82b4c55ace1f5a88129021959794f6ba4fc93225
Bhanuprasad <bhanuprasad.mende@effone.com>
2019-10-01 12:41:22 +0530

    build

---
+++ build/precache-manifest.031e3a48803e9a838be325df1868a185.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0089a75921851868fe2c26a249f0f7ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "185fc6161a2d9afa3a54",
+    "url": "/static/css/main.e8519ef1.chunk.css"
+  },
+  {
+    "revision": "16874122219191daf052",
+    "url": "/static/js/2.82f64fff.chunk.js"
+  },
+  {
+    "revision": "185fc6161a2d9afa3a54",
+    "url": "/static/js/main.6bc0d53f.chunk.js"
+  },
+  {
+    "revision": "1041b5cddc592f8caa32",
+    "url": "/static/js/runtime~main.9d18e353.js"
+  }
+]);
